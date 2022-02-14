4d8e4daa79d91831227c2eff77afdd566be9aa6c
David <davidjspinto92@gmail.com>
2022-02-14 18:03:54 +0000

    pushing everything

--- front-end/src/Pages/SingleFacility/index.js
+++ front-end/src/Pages/SingleFacility/index.js
@@ -3,6 +3,7 @@ import {Link, useParams} from "react-router-dom"
 import {Navbar, Sidebar} from "../../Components";
 import { useGlobalContext } from "../../Context/appContext";
 import "./singleFacility.css";
+import "../Dashboard/dashboard.css";
 
 const SingleFacility = () => {
    
@@ -11,11 +12,22 @@ const SingleFacility = () => {
         <div className="facilities-display">
             <Navbar />
             <div className="sidebar-and-content">
-            <Sidebar />
-            <p>Index {params.facilityName}
-           gggggggggggggggg
-            </p>
+                <Sidebar />
+                <h2>Facility {params.facilityName}</h2>
+                
             </div>
+            <div>
+                <p>Equipments and requisites</p>
+                <p>Last Legionella Analysis Results</p>
+                <p>Last IAQ Analysis Results</p>
+            </div>
+            <div className="actions-available">
+                <button className="actions-btn" type="button">Add Equipments and Systems</button>
+                <button className="actions-btn" type="button">Legionalla history</button>
+                <button className="actions-btn" type="button">QAI history</button>
+            </div>
+        
+
         </div>
     )
 }
